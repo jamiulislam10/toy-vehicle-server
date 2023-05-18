dbf07341688f1321eca2b7913b13bc8f9fb8bc28
jamiulislam10 <jamiulislam156@gmail.com>
2023-05-18 15:23:55 +0600

    add services collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,7 +36,16 @@ app.get('/services', async(req, res)=>{
     res.send(result);
 })
 
-
+app.get('/services/:id', async(req,res)=>{
+    const id =req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const options = {
+        // Include only the `title` and `imdb` fields in each returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+    };
+    const result = await serviceCollection.findOne(query,options);
+    res.send(result);
+})
 
 
     // Send a ping to confirm a successful connection
